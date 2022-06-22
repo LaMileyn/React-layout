6e3aaff0a4b9daffbbb34130c7bf9d5f5363f45a
Volg <lost-is-died@mail.ru>
2022-06-22 15:38:24 +0500

    start project ( created variables + fonts )

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./redux";
+import MainPage from "./pages/MainPage/MainPage";
+import './styles/variables.scss'
+import './styles/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}>
+                    <Route index element={<MainPage/>}/>
+                    <Route path={"services"} element={<Navigate to={<MainPage/>}/>}/>
+                    <Route path={"pricing"} element={<Navigate to={<MainPage/>}/>}/>
+                    <Route path={"blog"} element={<Navigate to={<MainPage/>}/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './MainPage.module.scss'
+
+const MainPage = (props) => {
+    return (
+        <section className={s['main-page']}>
+            hello
+        </section>
+    );
+}
+
+export default MainPage;
