957ddd909d6705e015ba3d42d865c61149c532d8
Volg <lost-is-died@mail.ru>
2022-06-22 16:59:26 +0500

    created 1.0 version topBanner

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import s from './MainPage.module.scss'
 import Header from "../../components/Header/Header";
+import TopBanner from "../../components/TopBanner/TopBanner";
 
 const MainPage = (props) => {
     return (
         <section className={s['main-page']}>
+            <TopBanner/>
         </section>
     );
 }
