ec31fef541df27dd79c9633eb2b761b64dba609b
Volg <lost-is-died@mail.ru>
2022-06-23 20:08:21 +0500

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import { React, useEffect } from 'react';
-import {Outlet} from "react-router-dom"
+import React from 'react';
+import { Outlet } from "react-router-dom"
 import Header from "./components/Header/Header";
-import {useDispatch} from "react-redux";
-import {getCards} from "./redux/reducers/CardsDataReducer";
+
 
 
 const App = () => {
-
-    // const dispatch = useDispatch()
-    // useEffect( () =>{
-    //     dispatch(getCards())
-    // },[])
-
 
     return (
         <div className="App">
