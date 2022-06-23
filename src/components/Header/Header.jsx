a78bc6777b9a4f8a63d05f8e59bbba0a61ca3f06
Volg <lost-is-died@mail.ru>
2022-06-23 16:29:03 +0500

    Изменена адаптивность в компоненте Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,47 @@
 import React from 'react';
 import s from './Header.module.scss';
-import cn from 'classnames';
 import {Link} from "react-router-dom";
 import Logotype from "../Icons/Logotype";
+import MediaQuery from 'react-responsive'
 
 const Header = (props) => {
     return (
         <div className={s.header}>
-            <div className={s.header__container}>
-                <div className={s.header__logo}>
-                    <div className={s.logo}>
-                        <Logotype/>
+            <MediaQuery minWidth={1041}>
+                <div className={s.header__container}>
+                    <div className={s.header__logo}>
+                        <div className={s.logo}>
+                            <Logotype/>
+                        </div>
+                        <h3>Agency</h3>
                     </div>
-                    <h3>Agency</h3>
+
+
+                    <nav className={s.header__nav}>
+                        <ul className={s.header__links}>
+                            <li><Link className={s.header__links} to={"/"}>About</Link></li>
+                            <li><Link className={s.header__links} to={"/"}>Services</Link></li>
+                            <li><Link className={s.header__links} to={"/"}>Pricing</Link></li>
+                            <li><Link className={s.header__links} to={"/"}>Blog</Link></li>
+                        </ul>
+                    </nav>
+
+
+                    <div className={s.header__btn}>
+                        <button>CONTACT</button>
+                    </div>
                 </div>
-
-
-                <nav className={s.header__nav}>
-                    <ul className={s.header__links}>
-                        <li><Link className={s.header__links} to={"/"}>About</Link></li>
-                        <li><Link className={s.header__links} to={"/"}>Services</Link></li>
-                        <li><Link className={s.header__links} to={"/"}>Pricing</Link></li>
-                        <li><Link className={s.header__links} to={"/"}>Blog</Link></li>
-                    </ul>
-                </nav>
-
-
-                <div className={s.header__btn}>
-                    <button>CONTACT</button>
+            </MediaQuery>
+            <MediaQuery maxWidth={1040}>
+                <div className={s['header__contaner-small-devices']}>
+                    <div className={s.header__logo}>
+                        <div className={s.logo}>
+                            <Logotype/>
+                        </div>
+                        <h3>Agency</h3>
+                    </div>
                 </div>
-            </div>
+            </MediaQuery>
         </div>
     );
 }
